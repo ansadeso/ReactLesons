9b9f99bfce3796ffab4c877876ef4db142554538
ansadeso <ansadeso@gmail.com>
2022-11-10 12:00:22 -0800

    11. add 2 foto

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import './App.css';
-import Header from './Header';
-import Technologies from './Technologies';
-import Footer from './Footer';
+import logos from './logo.jpg'
+import Nature from './Nature.jpg'
 
 const App = () => {
   return (
-    <div>
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src={logos} alt=''></img>
+      </header>
+      <nav className='nav'>
+        <div><a href='#s'>Profile</a></div>
+        <div><a href='#s'>Message</a></div>
+        <div><a href='#s'>News</a></div>
+        <div><a href='#s'>Music</a></div>     
+        <div><a href='#s'>Settings</a></div>     
+      </nav>
+      <div className='content'>
+        <div>
+          <img src={Nature}/>
+        </div>
+        <div>
+          ava + description
+        </div>
+        <div>
+          My posts
+          <div>
+            New Posts
+          </div>
+          <div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post 2
+            </div>
+          </div>
+        </div>
+      </div>
       
-      <Header />
-      <Technologies />
-      <Footer />
     </div>
   );
 }

e37f8ec2a1f0967d67b49c1bd126e2308e077b75
ansadeso <ansadeso@gmail.com>
2022-11-12 09:48:49 -0800

    20 NavLink

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
+import { NavLink } from 'react-router-dom';
 import s from './Navbar.module.css';
 const Navbar = () => {
     return(
         <nav className={s.nav}>
-        <div><a className={s.item} href='/profile'>Profile</a></div>
-        <div><a className={`${s.item} ${s.active}`} href='/dialogs'>Message</a></div>
+        <div><NavLink className={s.item} to='/profile'>Profile</NavLink></div>
+        <div><NavLink className={s.item} to='/dialogs'>Message</NavLink></div>
         <div><a className={s.item} href='#s'>News</a></div>
         <div><a className={s.item} href='#s'>Music</a></div>     
         <div><a className={s.item} href='#s'>Settings</a></div>     

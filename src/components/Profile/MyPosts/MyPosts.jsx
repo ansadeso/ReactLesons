11ffffef44ab5fd7fc224a165a3a5e918670bedc
ansadeso <ansadeso@gmail.com>
2022-11-13 10:56:34 -0800

    24. Refactoring data

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,10 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = () => {
+    let postData = [
+        {id: 1, message: 'Hi, how are you?', likesCount:'12'},
+        {id: 2, message: 'Its my first post', likesCount:'11'}
+    ];
     return(    
             <div className={s.postsBlock}>
                 <h3>My posts</h3>
@@ -12,8 +16,8 @@ const MyPosts = () => {
                     
                 </div>
                 <div className={s.posts}>
-                    <Post message='Hi, how are you?' likeCount='0' />
-                    <Post message='Its my first post' likeCount='23' />                    
+                    <Post message={postData[0].message} likeCount={postData[0].likesCount} />
+                    <Post message={postData[1].message} likeCount={postData[1].likesCount} />                                       
                 </div>
             </div>      
     );

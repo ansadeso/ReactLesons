0b7fe04ca30752e5b2ff8b1b9fe1c1083bb954e4
ansadeso <ansadeso@gmail.com>
2022-11-13 03:45:49 -0800

    20. gold link in css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import s from './Navbar.module.css';
 const Navbar = () => {
     return(
         <nav className={s.nav}>
-        <div><NavLink className={s.item} to='/profile'>Profile</NavLink></div>
+        <div><NavLink  className={s.item} to='/profile' activeClassName={s.activeLink}>Profile</NavLink></div>
         <div><NavLink className={s.item} to='/dialogs'>Message</NavLink></div>
         <div><a className={s.item} href='#s'>News</a></div>
         <div><a className={s.item} href='#s'>Music</a></div>     
